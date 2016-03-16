60e46e8fa6fcaa4d927ea441054230eecd67c2b8
tilios <tylerilunga1@gmail.com>
2016-03-15 21:09:43 -0700

    Changes to app.js

--- nexuData/app.js
+++ nexuData/app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
 
-mongoose.connect();
+mongoose.connect('mongodb://tylerilunga1:cougarsc7@ds011399.mlab.com:11399/iosdata');
 
+app.use('/', function(req, res, next){
+	console.log('Requested URL: ' + req.url);
+	//run the next middleware
+	next();
+})
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
@@ -18,6 +24,7 @@ var releaseItemSchema= new Schema({
 });
 
 var stockSchema = new Schema({
+	brand: String,
 	amount: Number
 });
 
@@ -29,61 +36,76 @@ var user = mongoose.model('user', userSchema);
 var stock = mongoose.model('stock', stockSchema);
 var limit = mongoose.model('limit', purchaseLimitSchema);
 
-var john = user({
-  firstname: 'John',
-  lastname: 'Doe',
-  address: '555 Main St.'
-});
 
-// save the user
-john.save(function(err) {
-  if (err) throw err;
+//socket.io
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+io.on('connection', function(socket) {
+	socket.on('connect', function() { 
+   		 console.log('connected');
+ });
 
-  console.log('person saved!');
-});
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 
-var jane = user({
-  firstname: 'Jane',
-  lastname: 'Doe',
-  address: '555 Main St.'
-});
+    socket.on('stock init', function(data) {
+        console.log('stock init called!');
+        var yeezy = stock({
+			brand: "Addias",
+			amount: 5
+		});
 
-// save the user
-jane.save(function(err) {
-  if (err) throw err;
+		yeezy.save(function(err){
+			if (err) throw err
 
-  console.log('person saved!');
-});
+			console.log("item saved!");
+		});
+        stock.find({}).limit(100).exec(function(err, data){
+            socket.emit('stock init', data);
+        });
+    });
 
-var apiController = require('./controllers/apiController');
-var htmlController = require('./controllers/htmlController');
+    socket.on('stock stored', function(data) {
+        console.log('Stock stored!');
 
-var port = process.env.PORT || 3000;
-
-app.use('/assets', express.static(__dirname + '/public'));
-
-app.set('view engine', 'ejs');
-
-app.use('/', function (req, res, next) {
-	console.log('Request Url:' + req.url);
-	
-	// get all the users
-	user.find({}, function(err, users) {
-		if (err) throw err;
 		
-		// object of all the users
-		console.log(users);
-	});
-	
-	next();
+    });
 });
 
-htmlController(app);
-
-apiController(app);
-
-app.listen(port, function(err){
+http.listen(port,"192.168.0.102", function(err){
 	if (err) throw err 
 
-	console.log("listening on port " + port);
+	console.log("listening on 192.168.0.102:" + port);
 });
+//web connection
+// 192.168.0.255
+// var apiController = require('./controllers/apiController');
+// var htmlController = require('./controllers/htmlController');
+
+// var port = process.env.PORT || 3000;
+
+// app.use('/assets', express.static(__dirname + '/public'));
+
+// app.set('view engine', 'ejs');
+
+// app.use('/', function (req, res, next) {
+// 	console.log('Request Url:' + req.url);
+	
+// 	// get all the users
+// 	user.find({}, function(err, users) {
+// 		if (err) throw err;
+		
+// 		// object of all the users
+// 		console.log(users);
+// 	});
+	
+// 	next();
+// });
+
+// htmlController(app);
+
+// apiController(app);
+
+
+
