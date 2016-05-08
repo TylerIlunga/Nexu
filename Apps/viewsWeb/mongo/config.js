7d12ee65fe500402fae0fd1f74229e6513b48dbf
tilios <tylerilunga1@gmail.com>
2016-05-08 14:52:56 -0700

    First Commit

---
+++ Apps/viewsWeb/mongo/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	"database" : "mongodb://*******:********7@ds021761.mlab.com:21761/views"
+}
---
+++ Apps/viewsWeb/public/controllers/error.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+	errApp = angular.module("error", []),
+	app = express();
+
+app.controller("404", ['$scope', function($scope){
+	var error = function(){
+		
+	}
+}]);
